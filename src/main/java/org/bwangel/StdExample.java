110112c74a8e6b0a2f6a4709615728b4d4ff2bd6
bwangelme <bwangel.me@gmail.com>
2022-04-23 07:14:16 +0800

    add dependency

---
+++ src/main/java/org/bwangel/StdExample.java
@@ -0,0 +1,10 @@
+package org.bwangel;
+
+import edu.princeton.cs.algs4.StdRandom;
+
+public class StdExample {
+    public static void main(String[] args) {
+        Double res = StdRandom.uniform();
+        System.out.println(res);
+    }
+}
--- src/test/java/org/bwangel/AppTest.java
+++ src/test/java/org/bwangel/AppTest.java
@@ -1,6 +1,5 @@
 package org.bwangel;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
