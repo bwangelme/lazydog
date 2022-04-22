07c36a1a2a9823b10e4283e4de8215cb569d299c
bwangelme <bwangel.me@gmail.com>
2022-04-23 05:33:31 +0800

    add example

---
+++ src/main/java/org/bwangel/App.java
@@ -0,0 +1,44 @@
+package org.bwangel;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Chapter 1 Example");
+    }
+
+    public static Integer sum(Integer a, Integer b) {
+        return a + b;
+    }
+
+    /**
+     * 判断整数 N 是否是素数
+     * @param N 整数N
+     * @return 是否是素数
+     */
+    public static Boolean IsPrime(Integer N) {
+        if (N < 2) {
+            return false;
+        }
+
+        for (int i = 2; i * i <= N; i++) {
+            if (N % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     *  计算数字 N 的平方根结果
+     *
+     *  牛顿迭代法
+     */
+    public static Double Sqrt(Double N) {
+        Double err = 1e-15;
+        Double t = N;
+        while( Math.abs(t-N/t) > err * t) {
+            t = (N/t + t) / 2.0;
+        }
+
+        return t;
+    }
+}
---
+++ src/test/java/org/bwangel/AppTest.java
@@ -0,0 +1,39 @@
+package org.bwangel;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void test_sum() {
+        assertEquals(Integer.valueOf(10), App.sum(3, 7));
+    }
+
+    @Test
+    public void test_IsPrime() {
+        assertTrue(App.IsPrime(3));
+        assertFalse(App.IsPrime(4));
+        assertTrue(App.IsPrime(37));
+        assertTrue(App.IsPrime(47));
+        assertTrue(App.IsPrime(67));
+        assertTrue(App.IsPrime(97));
+        assertTrue(App.IsPrime(89));
+        assertTrue(App.IsPrime(103));
+        assertTrue(App.IsPrime(107));
+    }
+
+    @Test
+    public void test_Sqrt() {
+        assertEquals(Double.valueOf(4), App.Sqrt(16.0));
+        assertEquals(Double.valueOf(3.0), App.Sqrt(9.0));
+        assertEquals(Double.valueOf(9.0), App.Sqrt(81.0));
+    }
+}
