e9e9d84b7979f9b28f2b5c2f5e9d550e7b3af506
bwangelme <bwangel.me@gmail.com>
2022-05-31 08:10:09 +0800

    add exercise 1.3.49

---
+++ src/main/java/org/bwangel/chapter1/ch1_3/SixStackQueue.java
@@ -0,0 +1,12 @@
+package org.bwangel.chapter1.ch1_3;
+
+/**
+ * 习题 1.3.49
+ *
+ * 用有限个栈实现队列，保证每次队列操作的时间复杂度(最坏情况下也是)都是常数级 O(1)
+ *
+ * 参考链接1: https://www.cnblogs.com/dacc123/p/10574939.html
+ * 参考链接2: https://corresponding.github.io/2018/02/06/20180206-stack-become-queue/
+ */
+public class SixStackQueue {
+}
